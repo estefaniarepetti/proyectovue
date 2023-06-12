397493a473d19ab8e5cea24ba6b12bdfcb11cd11
Estefania Repetti - Travel Services <repetti.stefania@gmail.com>
2023-06-12 16:25:44 -0300

    entrega De Vue CDN a Vue CLI

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="App vue"/>
+    <EntregaComp /> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import EntregaComp from './components/EntregaComp.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    EntregaComp
   }
 }
 </script>
